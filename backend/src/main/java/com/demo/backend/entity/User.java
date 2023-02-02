ea9fc69122ce5a30a9c4d3946d86020cd7b87163
plzdontcry19 <namsbw@hotmail.co.th>
2023-02-02 20:44:35 +0700

    feat: poc unit testing - service layer

--- backend/src/main/java/com/demo/backend/entity/User.java
+++ backend/src/main/java/com/demo/backend/entity/User.java
@@ -6,9 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @Entity()
 @Table(name = "users")
 public class User {
---
+++ backend/src/test/java/com/demo/backend/service/UserServiceTest.java
@@ -0,0 +1,52 @@
+package com.demo.backend.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.demo.backend.entity.User;
+import com.demo.backend.repository.UserRepository;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setup() {
+        User user = User.builder()
+                .age(1)
+                .name("Helo")
+                .userId(1L)
+                .build();
+
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        Mockito.when(this.userRepository.findByName("Helo")).thenReturn(users);
+    }
+
+    @Test
+    @DisplayName("Get Data based on Validate user name")
+    // @Disabled
+    public void whenValidUserName_thenUserShouldFound() {
+        String userName = "Helo";
+        List<User> users = this.userService.listUsersByName(userName);
+
+        for (User user : users) {
+            assertEquals(userName, user.getName());
+        }
+    }
+}
