e2dc0859d28dfd279fada1dd9416ba2382bf26d6
plzdontcry19 <namsbw@hotmail.co.th>
2023-02-06 13:53:25 +0700

    feat: poc actuator and custom Actuator

---
+++ backend/src/main/java/com/demo/backend/config/FeatureEndpoint.java
@@ -0,0 +1,38 @@
+package com.demo.backend.config;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Component
+@Endpoint(id = "features")
+public class FeatureEndpoint {
+
+    private final Map<String, Feature> featureMap = new ConcurrentHashMap<>();
+
+    @ReadOperation
+    public Map<String, Feature> FeatureEndpoint() {
+        featureMap.put("User", new Feature(true));
+        return featureMap;
+    }
+
+    @ReadOperation
+    public Feature features(@Selector String name) {
+        return featureMap.get(name);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Feature {
+        private Boolean isEnabled;
+    }
+}
